b3aee33b0228452685e5e5abd0a8eb6cc1d1df38
Miguel Angel Cumpa Ascuña <themiseck.rock@gmail.com>
2012-07-01 20:48:11 -0500

    delete persona

--- persona/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-                                                          
-from django.contrib import admin                                                 
-from models import Persona
-from forms import PersonaForm
-
-class PersonaAdmin(admin.ModelAdmin):
-    form=PersonaForm
-
-admin.site.register(Persona, PersonaAdmin)
--- persona/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from ubigeo.models import Ubigeo
-from ubigeo.fields import UbigeoField
-
-class Persona(models.Model):
-    name = models.CharField(max_length=150)
-    ubigeo = models.ForeignKey(Ubigeo)
